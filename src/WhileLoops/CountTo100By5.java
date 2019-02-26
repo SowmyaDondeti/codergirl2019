add8f7bebbfd88870ff3941d62c59f131b44554f
SowmyaDondeti <sowmyar987@gmail.com>
2019-02-26 14:11:21 -0600

    Change

--- src/WhileLoops/CountTo100By5.java
+++ src/WhileLoops/CountTo100By5.java
@@ -3,14 +3,12 @@ package WhileLoops;
 public class CountTo100By5
 {
   public static void main(String[] args) {
-    boolean keepgoing = true;
+
     int count = 0;
-    while (keepgoing) {
+    while (count<100) {
       count = count + 5;
       System.out.println(count);
-      if (count == 100) {
-        keepgoing = false;
-      }
+
     }
   }
 }
