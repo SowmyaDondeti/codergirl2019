3ede667091925e3f552d96d7e1d804806eb216a9
SowmyaDondeti <sowmyar987@gmail.com>
2019-02-26 21:00:47 -0600

    Added play again condition

--- src/DoWhile/GuessMyNumber.java
+++ src/DoWhile/GuessMyNumber.java
@@ -1,30 +1,41 @@
 package DoWhile;
 
-
 import java.util.Random;
 import java.util.Scanner;
 
-public class GuessMyNumber
-{
-    public static void main(String[] args){
-        Random num =new Random(2);
-       int  guessNum = num.nextInt(100);
-         System.out.println("I'm thinking of a number between 1 and 100. what is it?");
-        Scanner keyboard = new Scanner(System.in);
-        System.out.print("Guess:");
-        int number;
-        do{
-            number = keyboard.nextInt();
-            if (number <1 || number>100){
-                System.out.println("Oops! That number isn't between 1 and 100. Try again");
-            }else if (number > guessNum){
-             System.out.println("My number is lower");
-            }else if (number < guessNum){
-                 System.out.println("My number is higher");
-            }else{
-              System.out.println("you got it. my number was "+guessNum);
-            }
+public class GuessMyNumber {
+  public static void main(String[] args) {
+    boolean startGame = true;
+    Random num = new Random(2);
+    int number;
+    int guessNum = 0;
+    do {
+      if (startGame) {
+        guessNum = num.nextInt(100);
+        System.out.println("I'm thinking of a number between 1 and 100. what is it?");
+        startGame = false;
+      }
 
-        }while (number != guessNum);
-    }
+      Scanner keyboard = new Scanner(System.in);
+      System.out.print("Guess:");
+      number = keyboard.nextInt();
+      if (number < 1 || number > 100) {
+        System.out.println("Oops! That number isn't between 1 and 100. Try again");
+      } else if (number > guessNum) {
+        System.out.println("My number is lower");
+      } else if (number < guessNum) {
+        System.out.println("My number is higher");
+      } else {
+        System.out.println("you got it. my number was " + guessNum);
+        System.out.println("Play again?");
+        keyboard.skip("\n");
+        String userInput = keyboard.nextLine();
+        if (userInput.equals("yes")){
+            number = 0;
+            startGame = true;
+        }
+      }
+
+    } while (number != guessNum);
+  }
 }
