1969ca24c03f47f1a0053a97507689b99f12f987
SowmyaDondeti <sowmyar987@gmail.com>
2019-03-01 14:31:56 -0600

    Multiplication Table

---
+++ src/ForLoops/MultiplicationTables.java
@@ -0,0 +1,18 @@
+package ForLoops;
+
+import java.util.Scanner;
+
+public class MultiplicationTables {
+  public static void main(String[] args) {
+    Scanner keyboard = new Scanner(System.in);
+    System.out.print("How large would you like to see them?");
+    int size = keyboard.nextInt();
+    if (size<=20){
+    for (int i = 0; i <= size; i++) {
+        for (int j = 0; j <= size; j++) {
+          System.out.println(i + " * " + j + " = " + (i * j));
+        }
+      }
+    }
+  }
+}
