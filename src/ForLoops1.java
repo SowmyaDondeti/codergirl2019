d630006f3a12ea3b0ef6f2b5069187f8f80158d3
SowmyaDondeti <sowmyar987@gmail.com>
2019-03-01 12:39:42 -0600

    For loop program

---
+++ src/ForLoops1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ForLoops1 {
+  public static void main(String[] args) {
+    Scanner keyboard = new Scanner(System.in);
+    System.out.println("How big of a number should I count to?");
+    int input = keyboard.nextInt();
+    for (int i=1;i<=input;i++){
+      System.out.print(i+" ");
+    }
+      System.out.println("\n");
+    for (int i = input;i>=1;i--){
+      System.out.print(i+" ");
+    }
+  }
+}
