07cafa2d08f3869bfe77b3bd8502560c8f0d218f
SowmyaDondeti <sowmyar987@gmail.com>
2019-03-01 12:23:50 -0600

    Used for loop

--- src/WhileLoops/CountTo100By5.java
+++ src/WhileLoops/CountTo100By5.java
@@ -4,9 +4,7 @@ public class CountTo100By5
 {
   public static void main(String[] args) {
 
-    int count = 0;
-    while (count<100) {
-      count = count + 5;
+    for (int count =0;count<=100;count+=5){
       System.out.println(count);
 
 
