a0cec0a02618da5ee9b30ae581ebac9022435045
SowmyaDondeti <sowmyar987@gmail.com>
2019-03-03 06:38:53 -0600

    Multiplication Table Grid

--- src/ForLoops/MultiplicationTablesGrid.java
+++ src/ForLoops/MultiplicationTablesGrid.java
@@ -7,21 +7,21 @@ public class MultiplicationTablesGrid {
       int N=keyboard.nextInt();
     System.out.print("    ");
     for (int i=0;i<=N;i++)
-      System.out.print(pad(i)+" ");
+      System.out.print(align(i)+" ");
       System.out.println();
       System.out.print("    ");
         for (int i=0;i<=N;i++)
             System.out.print("    ");
         System.out.println();
         for (int i=0;i<=N;i++){
-            System.out.print(pad(i)+" ");
+            System.out.print(align(i)+" ");
             for (int j=0;j<=N;j++){
-                System.out.print(pad(i*j)+" ");
+                System.out.print(align(i*j)+" ");
             }
             System.out.println();
         }
 }
-  public static String pad(int x){
+  public static String align(int x){
       String s;
       if (x<10) s="  "+x;
       else if (x<100) s=" "+x;
