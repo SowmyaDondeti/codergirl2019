1922ee9accd3da47851e8107a4cd327e8ccdc596
SowmyaDondeti <sowmyar987@gmail.com>
2019-03-07 13:08:10 -0600

    created method

--- src/ForLoops/MultiplicationTables.java
+++ src/ForLoops/MultiplicationTables.java
@@ -5,10 +5,13 @@ import java.util.Scanner;
 public class MultiplicationTables {
   public static void main(String[] args) {
     Scanner keyboard = new Scanner(System.in);
-    System.out.print("How large would you like to see them?");
+    System.out.println("How large would you like to see them?");
     int size = keyboard.nextInt();
+    table(size);
+  }
+  public static void table(int size){
     if (size<=20){
-    for (int i = 0; i <= size; i++) {
+      for (int i = 0; i <= size; i++) {
         for (int j = 0; j <= size; j++) {
           System.out.println(i + " * " + j + " = " + (i * j));
         }
