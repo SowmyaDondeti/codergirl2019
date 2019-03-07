654972e469bdfefc778ad31c77bb7f7a302aaeef
SowmyaDondeti <sowmyar987@gmail.com>
2019-03-07 13:05:34 -0600

    changed method names

--- src/ForLoops/MultiplicationTablesGrid.java
+++ src/ForLoops/MultiplicationTablesGrid.java
@@ -6,9 +6,9 @@ public class MultiplicationTablesGrid {
       public static void main(String[] args) {
           Scanner keyboard = new Scanner(System.in);
           int number = keyboard.nextInt();
-          forLoop(number);
-          forLoop1(number);
-          forLoop3(number);
+          row(number);
+          rowAlign(number);
+          multiplication(number);
       }
 
       public static String align(int x) {
@@ -22,21 +22,21 @@ public class MultiplicationTablesGrid {
           }
           return s;
       }
-      public static void forLoop(int number){
+      public static void row(int number){
           System.out.print("    ");
           for (int i = 0; i <= number; i++) {
               System.out.print(align(i) + " ");
           }
           System.out.println();
       }
-      public static void forLoop1(int number){
+      public static void rowAlign(int number){
           System.out.print("    ");
           for (int i=0;i<=number;i++){
               System.out.print("    ");
           }
           System.out.println();
       }
-      public static void forLoop3(int number){
+      public static void multiplication(int number){
           for (int i=0;i<=number;i++){
               System.out.print(align(i)+" ");
               for (int j=0;j<=number;j++){
