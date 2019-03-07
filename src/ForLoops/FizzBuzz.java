740ecc2c5753c575a947f10827d2b35200ba7dc8
SowmyaDondeti <sowmyar987@gmail.com>
2019-03-06 19:16:50 -0600

    changed to single line

--- src/ForLoops/FizzBuzz.java
+++ src/ForLoops/FizzBuzz.java
@@ -6,7 +6,7 @@ public class FizzBuzz {
   }
   public static void fizzbuzz(){
     for (int i = 1; i <= 100; i++) {
-      if (i % 3 == 0 && i % 5 == 0) {
+      if (i % 15==0) {
         System.out.println("fizzbuzz");
       } else if (i % 3 == 0) {
         System.out.println("fizz");
