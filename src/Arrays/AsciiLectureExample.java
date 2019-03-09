19c8c0e06c6924af6c2fe6e20eb391b5e11ea009
SowmyaDondeti <sowmyar987@gmail.com>
2019-03-09 14:46:00 -0600

    Ascii program changed to uppercase letters

--- src/Arrays/AsciiLectureExample.java
+++ src/Arrays/AsciiLectureExample.java
@@ -1,8 +1,22 @@
 package Arrays;
 
-public class AsciiLectureExample
-{
-    public static void main(String[] args){
+public class AsciiLectureExample {
+  public static void main(String[] args) {
+    int nums[] = {67, 97, 116, 115, 32, 97, 114, 101, 32, 102, 117, 114, 114, 121, 33};
 
+    for (int index = 0; index < nums.length; index++) {
+        char letter;
+      if (nums[index] > 96) {
+
+         letter = (char) (nums[index]-32);
+
+
+      }else {
+          letter =(char)nums[index];
+      }
+        System.out.print(letter);
     }
+
+    System.out.println();
+  }
 }
