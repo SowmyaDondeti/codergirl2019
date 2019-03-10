8ac0424d549f8d5216bf4b69c56b97ce9deedf09
SowmyaDondeti <sowmyar987@gmail.com>
2019-03-09 19:26:39 -0600

    Created method

--- src/Arrays/AsciiLectureExample.java
+++ src/Arrays/AsciiLectureExample.java
@@ -3,18 +3,20 @@ package Arrays;
 public class AsciiLectureExample {
   public static void main(String[] args) {
     int nums[] = {67, 97, 116, 115, 32, 97, 114, 101, 32, 102, 117, 114, 114, 121, 33};
+    uppercase(nums);
+  }
 
+  public static void uppercase(int nums[]) {
     for (int index = 0; index < nums.length; index++) {
-        char letter;
+      char letter;
       if (nums[index] > 96) {
 
-         letter = (char) (nums[index]-32);
+        letter = (char) (nums[index] - 32);
 
-
-      }else {
-          letter =(char)nums[index];
+      } else {
+        letter = (char) nums[index];
       }
-        System.out.print(letter);
+      System.out.print(letter);
     }
 
     System.out.println();
