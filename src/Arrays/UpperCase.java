d57e0757ba3810e003e21a1fd994a62b655ee65f
SowmyaDondeti <sowmyar987@gmail.com>
2019-03-09 22:45:14 -0600

    Created uppercase method

--- src/Arrays/UpperCase.java
+++ src/Arrays/UpperCase.java
@@ -4,6 +4,9 @@ public class UpperCase
 {
     public static void main(String[] args){
         String message ="Why is Easter in the Spring?";
+        upperCase(message);
+    }
+    public static void upperCase( String message){
         char[] letter = message.toCharArray();
         for (int index=0;index<letter.length;index++){
             char character = letter[index];
