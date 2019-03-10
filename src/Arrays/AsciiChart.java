a04289d71d44a6c35928b2d3f112aa8295434fd7
SowmyaDondeti <sowmyar987@gmail.com>
2019-03-09 19:31:41 -0600

    Ascii chart

--- src/Arrays/AsciiChart.java
+++ src/Arrays/AsciiChart.java
@@ -3,6 +3,8 @@ package Arrays;
 public class AsciiChart
 {
     public static void main(String[] args){
-
+        for (int index=32;index<256;index++){
+      System.out.println((char)index);
+        }
     }
 }
