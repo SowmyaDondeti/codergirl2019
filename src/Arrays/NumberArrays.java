7cf3dadb2e685a418e723dc57fcef645ace8b3be
SowmyaDondeti <sowmyar987@gmail.com>
2019-03-09 19:21:58 -0600

    Created methods

--- src/Arrays/NumberArrays.java
+++ src/Arrays/NumberArrays.java
@@ -4,10 +4,12 @@ public class NumberArrays {
     int week1_temp[] = {65, 68, 72, 83, 73, 66, 85};
     int week2_temp[] = {86, 87, 85, 78, 79, 78, 80};
     String week_day[]={"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
+    average(week1_temp,week2_temp);
+    warmest(week1_temp,week2_temp,week_day);
+  }
+  public static void average(int week1_temp[],int week2_temp[]){
     int sum1 = 0;
     int sum2 = 0;
-    int big1 =0;
-    int big2 =0;
     for (int index = 0; index < week1_temp.length; index++) {
       sum1 += week1_temp[index];
       sum2 += week2_temp[index];
@@ -16,6 +18,10 @@ public class NumberArrays {
     float avg2 = (float) sum2 / week2_temp.length;
     System.out.println("The average daily high temperature for week1 is " + avg1);
     System.out.println("The average daily high temperature for week2 is " + avg2);
+  }
+  public static void warmest(int week1_temp[],int week2_temp[],String week_day[]){
+    int big1 =0;
+    int big2 =0;
     String day = "";
     String day1="";
     for (int index=0;index<week1_temp.length;index++){
@@ -30,9 +36,7 @@ public class NumberArrays {
         day1 = week_day[index];
       }
     }
-
     System.out.println("In week1,"+day+" was warmest,at " + big1 + " degrees");
-
     System.out.println("In week2,"+day1+" was warmest,at " + big2 + " degrees");
   }
 }
