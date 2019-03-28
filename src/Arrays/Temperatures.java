1d838fce8ec874f0d80ce4d4b9909718c73df58b
SowmyaDondeti <sowmyar987@gmail.com>
2019-03-27 19:57:36 -0500

    changes made

--- src/Arrays/Temperatures.java
+++ src/Arrays/Temperatures.java
@@ -16,17 +16,17 @@ public class Temperatures {
     String[] times = {"7AM", "3PM", "7PM", "3AM"};
     int[] timesSum = new int[4];
     int[] daysSum = new int[7];
-    int avg_of_week = 0;
+    int sum_of_week = 0;
     int count = 0;
     for (int i = 0; i < temps.length; i++) {
       for (int j = 0; j < temps[0].length; j++) {
-        avg_of_week += temps[i][j];
+        sum_of_week += temps[i][j];
         count++;
         timesSum[i] += temps[i][j];
         daysSum[j] += temps[i][j];
       }
     }
-    System.out.println("Average of whole week: " + avg_of_week / count);
+    System.out.println("Average of whole week: " + sum_of_week / count);
     for (int i = 0; i < times.length; i++) {
       System.out.println(times[i] + " : " + timesSum[i] / temps[i].length);
     }
